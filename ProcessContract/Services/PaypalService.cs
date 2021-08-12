5d404aed0c8daa6ff9d128ace57a2dca29a2af5f
diegolins10 <diegofernandeslins@gmail.com>
2021-08-12 18:29:56 -0300

    paypal service com a regra de negocio

---
+++ ProcessContract/Services/PaypalService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessContract.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        /*
+         * implementano a taxa de pagamento
+         * do paypal 2%
+         */
+        public double PaymentFee(double amount)
+        {
+            return amount * 1.02;
+        }
+
+        /*
+         *Juros simples
+         */
+        public double Interest (double amount, int month)
+        {
+            return (amount * (month / 100)) + amount;
+        }
+    }
+}
